1afbac100aa9e33561f52e69a9c37b4855c3a53c
zm <415741492@qq.com>
2022-02-12 17:15:11 +0800

    feat: add config(webpack,vuex,router)

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'haha'
+    }
+  }
+})
+
+export default store
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  //配置方式一：cli提供的属性
+  outputDir: './build',
+  //配置方式二：和webpack的属性完全一致，最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // },
+  // configureWebpack: (config) => {
+  //   //覆盖原来的alias配置
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // },
+  //配置方式三：链式编程
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
+}
